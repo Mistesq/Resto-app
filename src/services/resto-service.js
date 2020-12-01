b7cebf45bc152ce910842eead4347da1d8cd8f4d
Mistesqr <mistesqr@gmail.com>
2020-12-01 21:41:51 +0200

    server setup && routing

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,19 @@
-export default class RestoService {
-    getMenuItems() {
-        return [];
+
+export default class RestoService{
+    _apiBase = 'http://localhost:3004';
+
+    async getResource(url) {
+        const response = await fetch(`${this._apiBase}${url}`);
+
+        if (!response.ok){
+            throw new Error(`Could not fetch ${url}, received ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result
+    }
+
+    async getMenuItems() {
+        return await this.getResource(`/menu/`)
     }
 }
