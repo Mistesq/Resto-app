b7406c5d0857d73e98365ee81d72f75722b642ed
Mistesqr <mistesqr@gmail.com>
2020-12-02 21:42:40 +0200

    added cart total functional

--- src/components/cart-table/cart-table.js
+++ src/components/cart-table/cart-table.js
@@ -1,31 +1,50 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {deleteFromCart} from '../../actions';
+import WithRestoService from '../hoc';
+
 import './cart-table.scss';
 
-const CartTable = ({items, deleteFromCart}) => {
+const CartTable = ({RestoService, items, deleteFromCart}) => {
     return (
         <>
             <div className="cart__title">Ваш заказ:</div>
             <div className="cart__list">
                 {
                     items.map(item => {
-                        const {title, price, url, id} = item;
-
+                        const {title, price, url, id, quantity} = item;
+                        let quantityStr = ``;
+                        if (quantity > 1) {
+                            quantityStr = ` * ${quantity}`;
+                        }
+                        
                         return (
                             <div key={id} className="cart__item">
                                 <img src={url} className="cart__item-img" alt={title}></img>
                                 <div className="cart__item-title">{title}</div>
-                                <div className="cart__item-price">{price}$</div>
+                                <div className="cart__item-price">{price}${quantityStr}</div>
+                                <div className="cart__item-price">Total:{price * quantity}$</div>
                                 <div onClick={() => deleteFromCart(id)} className="cart__close">&times;</div>
                             </div>
                         )
                     })
                 }
             </div>
+            <button onClick = {() => {RestoService.setOrder( generateOrder(items))} } className="order">Оформить заказ</button>
         </>
     );
 };
+
+
+const generateOrder = (items) => {
+    const newOrder = items.map(item => {
+        return {
+            id: item.id,
+            qtty: item.qtty
+        }
+    })
+    return newOrder;
+}
 
 const mapStateToProps = ({items}) => {
     return {
@@ -36,4 +55,4 @@ const mapStateToProps = ({items}) => {
 const mapDispatchToProps = {
     deleteFromCart
 }
-export default connect(mapStateToProps, mapDispatchToProps)(CartTable);
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(CartTable));
